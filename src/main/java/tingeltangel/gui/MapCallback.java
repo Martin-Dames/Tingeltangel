747be4f73af0763d14ffabf79dba21204ecdcbf4
Martin Dames <martin.dames@pumacy.de>
2015-04-29 15:51:25 +0200

    added class MapCallback

---
+++ src/main/java/tingeltangel/gui/MapCallback.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tingeltangel.gui;
+
+import java.util.Map;
+
+/**
+ *
+ * @author mdames
+ */
+public interface MapCallback {
+    
+    public void callback(Map<String, Object> data);
+    
+}
