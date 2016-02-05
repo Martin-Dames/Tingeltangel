a9204b7b7f0aa7a39c66257468d8ac456a7d291b
Martin Dames <martin.dames@pumacy.de>
2016-02-05 13:03:15 +0100

    removed output redirection to tt.log from start scripts

--- Tingeltangel.sh
+++ Tingeltangel.sh
@@ -2,5 +2,4 @@
 
 JAR=tingeltangel-0.2-beta2-jar-with-dependencies.jar
 
-java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true \
-    -jar $JAR > tt.log 2>&1
+java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true -jar $JAR
