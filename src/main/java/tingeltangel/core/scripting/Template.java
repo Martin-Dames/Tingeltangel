f615fd28c75100a3dcbd7327b0c6dad3185ea849
Martin Dames <martin.dames@pumacy.de>
2016-02-09 16:12:19 +0100

    added exp2 template

--- src/main/java/tingeltangel/core/scripting/Template.java
+++ src/main/java/tingeltangel/core/scripting/Template.java
@@ -35,7 +35,7 @@ public class Template {
     private String name = "";
     private int labelCounter = 0;
     
-    private final static String[] TEMPLATES = {"mul", "div", "mod", "divmod", "min", "max"};
+    private final static String[] TEMPLATES = {"mul", "div", "mod", "divmod", "min", "max", "exp2"};
     private final static HashMap<String, Template> templates = new HashMap<String, Template>();
     
     static {
