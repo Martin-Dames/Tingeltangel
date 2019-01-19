f193cae50101a59a5dd9d2f9af33ce97a636dcb7
Piotr Daszko <piotr.daszko@gmail.com>
2019-01-19 21:22:23 +0100

    Set labels as constant variables

--- src/main/java/tingeltangel/gui/SearchPanel.java
+++ src/main/java/tingeltangel/gui/SearchPanel.java
@@ -18,35 +18,51 @@ import tingeltangel.gui.IndexListEntry;
 
 public class SearchPanel extends JPanel {
 
+    private String SEARCH_LABEL = "Suche";
+    private String CLEAR_LABEL = "Löschen";
+    private String MP3_TYPE = "MP3";
+    private String SCRIPT_TYPE = "Script";
+    private String SCRIPT_SUBROUTINE_TYPE = "Script (Subroutine)";
+    private String TTS_TYPE = "TTS (Text to speech)";
+    private String NAME_LABEL = "Name: ";
+    private String ID_LABEL = "ID: ";
+    private String TYPE_LABEL = "Type: ";
+    private String SEARCH_RESULTS_LABEL = "Ergebnisse der Suche: ";
+    private int SEARCH_FIELD_WIDTH = 100;
+    private int SEARCH_FIELD_HIGH =  20;
+
+
+
     private final static Logger log = LogManager.getLogger(SearchPanel.class);
 
     private JTextField name = new JTextField();
     private JTextField id = new JTextField();
-    private final String[] types = {"", "MP3", "Script", "Script (Subroutine)", "TTS (Text to speech)"};
+    private final String[] types = {"", MP3_TYPE, SCRIPT_TYPE, SCRIPT_SUBROUTINE_TYPE, TTS_TYPE};
     private JComboBox<String> type = new JComboBox<>(types);
 
-    private JButton search = new JButton("Search");
-    private JButton clear = new JButton("Clear");
+    private JButton search = new JButton(SEARCH_LABEL);
+    private JButton clear = new JButton(CLEAR_LABEL);
 
 
-    Border border = BorderFactory.createTitledBorder("Search");
+    Border border = BorderFactory.createTitledBorder(SEARCH_LABEL);
     public SearchPanel(EditorFrame mainFrame, EditorPanel editorPanel) {
         this.setBorder(this.border);
-        this.add(new JLabel("Name: "));
-        this.name.setPreferredSize(new Dimension(100, 20));
-        this.add(this.name);
 
-        this.add(new JLabel("ID: "));
-        this.id.setPreferredSize(new Dimension(100, 20));
+        this.add(new JLabel(ID_LABEL));
+        this.id.setPreferredSize(new Dimension(SEARCH_FIELD_WIDTH, SEARCH_FIELD_HIGH));
         this.add(this.id);
 
-        this.add(new JLabel("Type: "));
+        this.add(new JLabel(TYPE_LABEL));
         this.add(this.type);
 
+        this.add(new JLabel(NAME_LABEL));
+        this.name.setPreferredSize(new Dimension(SEARCH_FIELD_WIDTH, SEARCH_FIELD_HIGH));
+        this.add(this.name);
+
         this.search.addActionListener((ActionEvent se) -> {
-            log.info("Name : " + this.name.getText() + "\n"
-                    + "ID: " + this.id.getText() + "\n"
-                    + "type: " + this.type.getSelectedItem().toString()
+            log.info(NAME_LABEL + this.name.getText() + "\n"
+                    + ID_LABEL + this.id.getText() + "\n"
+                    + TYPE_LABEL + this.type.getSelectedItem().toString()
                     + "selected index type: " + this.type.getSelectedIndex());
             List<Entry> result = new ArrayList<>(mainFrame.getBook().getIndexEntries().values());
             if (!this.name.getText().isEmpty()) {
@@ -79,7 +95,7 @@ public class SearchPanel extends JPanel {
 
             editorPanel.getList()
                     .setBorder(
-                            BorderFactory.createTitledBorder("Search results: " + result.size()));
+                            BorderFactory.createTitledBorder(SEARCH_RESULTS_LABEL + result.size()));
 
             for (Entry entry: result) {
                 editorPanel.getList().add(new IndexListEntry(entry, editorPanel));
@@ -103,7 +119,7 @@ public class SearchPanel extends JPanel {
 
             editorPanel.getList()
                     .setBorder(
-                            BorderFactory.createTitledBorder("Search results: "
+                            BorderFactory.createTitledBorder( SEARCH_RESULTS_LABEL
                                     + mainFrame.getBook().getIndexEntries().size()));
 
             for (Entry entry: mainFrame.getBook().getIndexEntries().values()) {
