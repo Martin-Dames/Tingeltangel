7ce7fc5e0999db70b7b9cd8cbe03a55425faf4a2
mdames <mdames@T530-CKIND.pumacy.local>
2019-04-03 13:25:45 +0200

    added support for packaging with open_jre_1.8.0 for windows

--- TingeltangelCLI.bat
+++ TingeltangelCLI.bat
@@ -1,1 +1,5 @@
-java -jar tingeltangel-0.6-jar-with-dependencies.jar cli
+IF EXIST open_jre_1.8.0/ (
+	open_jre_1.8.0/bin/java -jar tingeltangel-0.6-jar-with-dependencies.jar cli
+) ELSE (
+	java -jar tingeltangel-0.6-jar-with-dependencies.jar cli
+)
--- TingeltangelEditor_noOfficialBooks.bat
+++ TingeltangelEditor_noOfficialBooks.bat
@@ -1,1 +1,5 @@
-start javaw -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true -jar tingeltangel-0.6-jar-with-dependencies.jar gui-editor disable-official-books
+IF EXIST open_jre_1.8.0/ (
+	start open_jre_1.8.0/bin/javaw -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true -jar tingeltangel-0.6-jar-with-dependencies.jar gui-editor disable-official-books
+) ELSE (
+	start javaw -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true -jar tingeltangel-0.6-jar-with-dependencies.jar gui-editor disable-official-books
+)
