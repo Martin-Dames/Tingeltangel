eb3e651f3b3f6712a1ec1f978e841e5edef94e6a
André Tietz <andre.tietz@gmail.com>
2020-12-22 12:29:04 +0100

    removing registry request in order to get the home dir of the user. (sui)

--- src/main/java/tingeltangel/tools/FileEnvironment.java
+++ src/main/java/tingeltangel/tools/FileEnvironment.java
@@ -23,18 +23,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.channels.FileChannel;
 import java.util.UUID;
 
 /**
- *
  * @author mdames
  */
 public class FileEnvironment {
-    
+
     private static boolean test = false;
-    
+
     /**
      * switch on test mode
      * to be able to import books without overwriting existing books
@@ -46,34 +44,34 @@ public class FileEnvironment {
     public static File getHomeDirectory() {
         return new File(System.getProperty("user.home"));
     }
-    
+
     public static File getAndersichtDirectory() {
         File f = getWorkingDirectory("andersicht");
-        if(!f.exists()) {
+        if (!f.exists()) {
             f.mkdir();
         }
-        return(f);
+        return (f);
     }
-    
+
     public static File getAndersichtBookDirectory(String name) {
         File f = new File(getAndersichtDirectory(), name);
-        if(!f.exists()) {
+        if (!f.exists()) {
             f.mkdir();
             new File(f, "audio").mkdir();
         }
-        return(f);
+        return (f);
     }
-    
+
     public static File getAndersichtBookFile(String name) {
-        return(new File(getAndersichtBookDirectory(name), "book.def"));
+        return (new File(getAndersichtBookDirectory(name), "book.def"));
     }
-    
+
     public static File getAndersichtAudioDirectory(String name) {
-        return(new File(getAndersichtBookDirectory(name), "audio"));
+        return (new File(getAndersichtBookDirectory(name), "audio"));
     }
 
     public static File getBooksDirectory() {
-        if(test) {
+        if (test) {
             return getWorkingDirectory("books-test");
         }
         return getWorkingDirectory("books");
@@ -100,32 +98,32 @@ public class FileEnvironment {
     public static File getRepositoryDirectory() {
         return getWorkingDirectory("repository");
     }
-    
+
 
     public static File getAudioDirectory(int id) {
-        return(getBookSubDirectory(id, "audio"));
+        return (getBookSubDirectory(id, "audio"));
     }
-    
+
     public static File getDistDirectory(int id) {
-        return(getBookSubDirectory(id, "dist"));
+        return (getBookSubDirectory(id, "dist"));
     }
-    
+
     public static File getBinDirectory(int id) {
-        return(getBookSubDirectory(id, "bin"));
+        return (getBookSubDirectory(id, "bin"));
     }
-    
+
     public static File getCodesDirectory(int id) {
-        return(getBookSubDirectory(id, "codes"));
+        return (getBookSubDirectory(id, "codes"));
     }
-    
+
     public static File getPagesDirectory(int id) {
-        return(getBookSubDirectory(id, "pages"));
+        return (getBookSubDirectory(id, "pages"));
     }
-    
+
     public static File getBinObjectFile(int mid, int oid) {
-        return(new File(getBinDirectory(mid), Integer.toString(oid) + ".bin"));
+        return (new File(getBinDirectory(mid), Integer.toString(oid) + ".bin"));
     }
-    
+
     private static File getBookSubDirectory(int id, String subDirectory) {
         File codes = new File(getBookDirectory(id), subDirectory);
         if (codes.exists()) {
@@ -139,11 +137,11 @@ public class FileEnvironment {
         }
         return codes;
     }
-    
+
     public static File getXML(int id) {
-        return(new File(getBookDirectory(id), "book.xml"));
+        return (new File(getBookDirectory(id), "book.xml"));
     }
-    
+
     private static File getWorkingDirectory(String subDirectory) {
         File wd = new File(getWorkingDirectoryRoot(), subDirectory);
         if (wd.exists()) {
@@ -159,25 +157,7 @@ public class FileEnvironment {
     }
 
     public static File getWorkingDirectoryRoot() {
-        File wd = null;
-        if (OS.isWindows()) {
-            String myDocuments = null;
-            try {
-                Process p = Runtime.getRuntime().exec("reg query \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders\" /v personal");
-                p.waitFor();
-                InputStream in = p.getInputStream();
-                byte[] b = new byte[in.available()];
-                in.read(b);
-                in.close();
-                myDocuments = new String(b);
-                myDocuments = myDocuments.split("\\s\\s+")[4];
-            } catch (Exception e) {
-                myDocuments = ".";
-            }
-            wd = new File(myDocuments, "tingeltangel");
-        } else {
-            wd = new File(getHomeDirectory(), ".tingeltangel");
-        }
+        File wd = new File(getHomeDirectory(), ".tingeltangel");
         if (wd.exists()) {
             if (!wd.isDirectory()) {
                 throw new Error(wd.getAbsolutePath() + " exists but is not a directory");
@@ -192,12 +172,12 @@ public class FileEnvironment {
 
     public static File getFreeFileName(File directory, String fileExtension) {
         File f = new File(directory, UUID.randomUUID().toString() + fileExtension);
-        while(f.exists()) {
+        while (f.exists()) {
             f = new File(directory, UUID.randomUUID().toString() + fileExtension);
         }
-        return(f);
+        return (f);
     }
-    
+
     public static void copy(File source, File destination) throws IOException {
         FileChannel sourceChannel = null;
         FileChannel destChannel;
@@ -210,13 +190,13 @@ public class FileEnvironment {
         } catch (Exception e) {
             throw new IOException(e);
         } finally {
-            if(sourceChannel != null) {
+            if (sourceChannel != null) {
                 sourceChannel.close();
             }
-            if(fout != null) {
+            if (fout != null) {
                 fout.close();
             }
         }
     }
-    
+
 }
